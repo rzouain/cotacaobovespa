fb48ca21e2fc52814d14acab1293e108db3615d1
Moises Trovo <moises.trovo@gmail.com>
2010-04-09 16:57:50 -0300

    Changes to support BM&FBovespa quote provider

--- bovespa.py
+++ bovespa.py
@@ -1,31 +1,36 @@
 import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
-import yql
+from xml.dom import minidom
+from httplib2 import Http
 
 class QuoteHandler(webapp.RequestHandler):
-  
+  QUOTE_URL = 'http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp?intEstado=1&CodigoPapel=%s'
+  http = Http()
   def get(self):
       stock = self.request.get("stock")
       stock = stock.upper()
-      y = yql.Public()
-      q = "select * from html where xpath='//td[@class=\"tdValor\"]/p' and url='http://www.bmfbovespa.com.br/Cotacao-Rapida/ExecutaAcaoCotRapXSL.asp?gstrCA=&txtCodigo="+stock+"&intIdiomaXsl=0'"
-      r = y.execute(q)
-    
-      if len(r.rows)>0:
-        preco = r.rows[0]
-        erro = "0"
-      elif len(r.rows)<=0:
-        preco = "0"
-        erro = "1"
-    
+      (res, content) = self.http.request(self.QUOTE_URL % stock)
+      xml = minidom.parseString(content)
+      ps = xml.getElementsByTagName('Papel')
+      
+      preco = None
+      erro = None
+      if not ps or len(ps) == 0:
+        erro = '1'
+      else:
+        erro = '0'
+        papel = ps[0]
+        preco = papel.getAttribute('Ultimo')
+      
       template_values = {
        'preco': preco,
        'erro': erro,
        'stock': stock,
       }
-   
+
       path = os.path.join(os.path.dirname(__file__), 'templates/cotacao_bovespa.html')
       self.response.out.write(template.render(path, template_values))
 
